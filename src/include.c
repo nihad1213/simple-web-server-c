66fae11af50acefba9e5a5ee4becb97b89d19001
Nihad <nihad.nemetli@gmail.com>
2026-08-26 21:52:10 +0400

    new: test files created

---
+++ src/include.c
@@ -0,0 +1,5 @@
+#include "include.h"
+
+int add(int a, int b) {
+    return a + b;
+}
---
+++ src/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "include.h"
+
+int main() {
+
+    printf("Test");
+    add(5, 12);
+
+    return 0;
+}
