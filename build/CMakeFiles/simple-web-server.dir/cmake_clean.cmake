6935174d2d5691f9837a85a5e564e2d0555eb1f5
Nihad <nihad.nemetli@gmail.com>
2026-08-26 21:56:42 +0400

    new: CMaleLists.txt file created

---
+++ build/CMakeFiles/simple-web-server.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simple-web-server.dir/link.d"
+  "CMakeFiles/simple-web-server.dir/src/include.c.o"
+  "CMakeFiles/simple-web-server.dir/src/include.c.o.d"
+  "CMakeFiles/simple-web-server.dir/src/main.c.o"
+  "CMakeFiles/simple-web-server.dir/src/main.c.o.d"
+  "simple-web-server"
+  "simple-web-server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/simple-web-server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
