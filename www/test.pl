ef454ac6b2cb6ac9f217a17aee95400c4bca2982
Nihad <nihad.nemetli@gmail.com>
2026-08-29 11:31:37 +0400

    new: www files created

---
+++ www/test.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+print "Content-Type: text/html\n\n";
+
+print <<"EOF";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>This is simple Perl file</p>
+</body>
+</html>
+EOF
